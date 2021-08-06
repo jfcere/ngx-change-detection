dd2882fa9b1583a4d290a0828f9374b03b44d6e9
jfcere <jfcere@sherweb.com>
2021-08-06 13:54:55 -0400

    Add EsLint configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ParentOnpushComponent } from './parent-onpush/parent-onpush.component';
 import { ChildDefaultComponent } from './child-default/child-default.component';
 import { GrandchildDefaultComponent } from './grandchild-default/grandchild-default.component';
+import { ParentOnpushComponent } from './parent-onpush/parent-onpush.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentOnpushComponent,
     ChildDefaultComponent,
-    GrandchildDefaultComponent
+    GrandchildDefaultComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
